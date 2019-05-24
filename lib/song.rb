75e8b212df7a12f2cfe0e0c29b951b7d8ec94dd8
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-24 02:41:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     artist_name = filename.split(" - ")[0]
     song_name = filename.split("-")[1].strip
     song = Song.new(song_name)
-    artist = Artist.find_or_create_by_name
+    artist = Artist.find_or_create_by_name(artist_name)
     song.artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(song)
     return song
